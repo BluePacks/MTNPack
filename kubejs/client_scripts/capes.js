194d27044494db1ac5280c3cdbd83701562eb91a
Bluemethyst <88925092+Bluemethyst@users.noreply.github.com>
2024-05-08 21:43:08 +1200

    Update capes.js

--- kubejs/client_scripts/capes.js
+++ kubejs/client_scripts/capes.js
@@ -1,5 +1,5 @@
 CapeJS.addCapes((event) => {
-    let users = ["607c455c-a7de-4030-adf0-363cf08fd54b"];
+    let users = ["607c455c-a7de-4030-adf0-363cf08fd54b"]; // add uuid into this array
     for (let user of users) {
         event.register(user, "mountain");
     }
